75bca0340d1ee6a8eee4a09c9efc7be44120da5a
Bastien B. <82913334+MiTiX1@users.noreply.github.com>
2024-09-21 22:54:55 +0200

    Revert "Feat/data generator"

--- data-generator/data_generator/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .accounts import AccountModel
-from .banks import BankModel
-from .transactions import TransactionModel
-from .users import UserModel
-from .common import session, AccountStatusEnum, AccountTypeEnum, ActionEnum, TransactionStatusEnum, TransactionTypeEnum
